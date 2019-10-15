17a23dee7e946192117d53be325b76b41c61eb1e
Audrow Nash <audrowna@usc.edu>
2019-10-14 20:48:51 -0700

    Add function to split Dataframes on NaN

--- robotpt_common_utils/pandas_lib.py
+++ robotpt_common_utils/pandas_lib.py
@@ -1,23 +1,25 @@
 import pandas as pd
+import numpy as np
 
 
 def remove_consecutive_nans(
         dataframe,
         column_name,
-        max_consecutive_nans_before_remove=2
+        max_consecutive_nans
 ):
     if type(dataframe) is not pd.DataFrame:
         raise TypeError("'dataframe' should be a Pandas Dataframe")
-    if max_consecutive_nans_before_remove < 0:
+    if max_consecutive_nans < 0:
         raise ValueError("'max_consecutive_nans' should be greater than 0")
 
+    # Same as function below but doesn't work if this is placed in a function
     _dataframe = dataframe.copy()
     _dataframe['Group'] = _dataframe[column_name].notnull().astype(int).cumsum()
     _dataframe = _dataframe[_dataframe[column_name].isnull()]
     _dataframe = _dataframe[
         _dataframe.Group.isin(
             _dataframe.Group.value_counts()[
-                _dataframe.Group.value_counts() > max_consecutive_nans_before_remove
+                _dataframe.Group.value_counts() <= max_consecutive_nans
                 ].index
         )
     ]
@@ -25,3 +27,19 @@ def remove_consecutive_nans(
     _dataframe.drop_duplicates(['Group'], keep='first')
 
     return dataframe.drop(_dataframe.index)
+
+
+def split_on_nan(dataframe, column_name):
+
+    dataframes = np.split(dataframe, np.where(np.isnan(dataframe[column_name]))[0])
+
+    # removing NaN entries
+    dataframes = [
+        ev[~np.isnan(ev[column_name])]
+        for ev in dataframes
+        if not isinstance(ev, np.ndarray)
+    ]
+
+    # removing empty DataFrames
+    dataframes = [ev for ev in dataframes if not ev.empty]
+    return dataframes
--- tests/test_pandas_lib.py
+++ tests/test_pandas_lib.py
@@ -5,9 +5,9 @@ import numpy as np
 from robotpt_common_utils import pandas_lib
 
 
-class TestPandasLib(unittest.TestCase):
+class TestRemoveConsecutiveNans(unittest.TestCase):
 
-    def test_remove_consecutive_nans_from_full_list(self):
+    def test_full_list(self):
         values = [1, 2, 3, 4, 5]
         df_in = pd.DataFrame({
             "Values":
@@ -20,10 +20,10 @@ class TestPandasLib(unittest.TestCase):
                     df_out.Values[i]
                 )
 
-    def test_remove_consecutive_nans_from_one_missing(self):
+    def test_one_missing(self):
         values_in = [1, np.nan, 3, 4, 5]
-        values_out1 = [1, 3, 4, 5]
-        values_out2 = values_in
+        values_out1 = values_in
+        values_out2 = [1, 3, 4, 5]
 
         df_in = pd.DataFrame({
             "Values":
@@ -31,10 +31,14 @@ class TestPandasLib(unittest.TestCase):
         df_out1 = pandas_lib.remove_consecutive_nans(df_in, 'Values', 0)
         test_out1 = list(df_out1.Values)
         for i in range(len(values_out1)):
-            self.assertEqual(
-                values_out1[i],
-                test_out1[i]
-            )
+            true_val = values_out1[i]
+            test_val = test_out1[i]
+            if np.isnan(true_val):
+                self.assertTrue(np.isnan(test_val))
+            else:
+                self.assertEqual(
+                    true_val, test_val
+                )
 
         df_out2 = pandas_lib.remove_consecutive_nans(df_in, 'Values', 1)
         test_out2 = list(df_out2.Values)
@@ -48,16 +52,16 @@ class TestPandasLib(unittest.TestCase):
                     true_val, test_val
                 )
 
-    def test_remove_consecutive_nans_with_multiple_missing(self):
+    def test_multiple_missing(self):
 
         values_in = [1, np.nan,
                      3, np.nan, np.nan,
                      4, np.nan, np.nan, np.nan,
                      5, np.nan, ]
-        values_out = [1, np.nan,
-                      3, np.nan, np.nan,
-                      4,
-                      5, np.nan, ]
+        values_out = [1,
+                      3,
+                      4, np.nan, np.nan, np.nan,
+                      5, ]
         df_in = pd.DataFrame({"Values": values_in})
 
         df_out = pandas_lib.remove_consecutive_nans(df_in, 'Values', 2)
@@ -70,4 +74,28 @@ class TestPandasLib(unittest.TestCase):
             else:
                 self.assertEqual(
                     true_val, test_val
+                )
+
+
+class TestSplitOnMain(unittest.TestCase):
+
+    def test_output(self):
+
+        values_in = [0,
+                     1, np.nan,
+                     2, np.nan, np.nan,
+                     3, np.nan, np.nan, np.nan,
+                     ]
+        values_out = [[0, 1],
+                      [2],
+                      [3]]
+        df_in = pd.DataFrame({"Values": values_in})
+        test_out = pandas_lib.split_on_nan(df_in, 'Values')
+
+        for i in range(len(values_out)):
+            true_val = values_out[i]
+            test_val = list(test_out[i]["Values"])
+            for j in range(len(true_val)):
+                self.assertEqual(
+                    true_val[j], test_val[j]
                 )
