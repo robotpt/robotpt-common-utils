9a4117214bff9b4bdc784fab9d28dc97d420b4ce
Audrow Nash <audrowna@usc.edu>
2019-10-17 18:44:00 -0700

    Create a function to get date ranges and output as datetime or strings

--- robotpt_common_utils/dates.py
+++ robotpt_common_utils/dates.py
@@ -1,4 +1,5 @@
 import datetime
+from robotpt_common_utils import math_tools
 
 NUM_DAYS_IN_A_WEEK = 7
 
@@ -21,3 +22,33 @@ def subtract_weeks(start_date, end_date, is_full_weeks_only=True):
         return int(weeks_difference)
     else:
         return weeks_difference
+
+
+def get_date_range(
+        start_date,
+        end_date,
+        increment_days=1,
+        output_format=None
+):
+    if not math_tools.is_int(increment_days):
+        raise ValueError("'increment_days' must be an int")
+
+    if increment_days < 0:
+        raise ValueError("'increment_days' must be nonnegative")
+
+    if start_date > end_date:
+        raise IOError("'start_date' must be less than 'end_date'")
+
+    date = start_date
+    date_range = []
+    while date < end_date:
+        date_range.append(date)
+        date += datetime.timedelta(days=increment_days)
+    if output_format is None:
+        return date_range
+    elif type(output_format) is str:
+        return [
+            datetime.datetime.strftime(date, output_format)
+            for date in date_range
+        ]
+
--- tests/test_dates.py
+++ tests/test_dates.py
@@ -50,3 +50,64 @@ class TestDates(unittest.TestCase):
                 datetime.date(2019, 1, 1),
             )
         )
+
+    def test_get_date_range_single_increment(self):
+        for range_length in range(10):
+            start_date = datetime.date(2019, 10, 1)
+            end_date = start_date + datetime.timedelta(days=range_length)
+            truth_date_range = [
+                start_date + datetime.timedelta(days=i) for i in range(range_length)
+            ]
+            test_date_range = dates.get_date_range(start_date, end_date)
+            self.assertEqual(
+                range_length,
+                len(test_date_range),
+            )
+            for i in range(range_length):
+                self.assertEqual(
+                    truth_date_range[i],
+                    test_date_range[i]
+                )
+
+    def test_get_date_range_multiple_increment(self):
+
+        for increment in range(1, 10):
+            range_length = 3
+            start_date = datetime.date(2019, 10, 1)
+            end_date = start_date + datetime.timedelta(days=range_length*increment)
+            truth_date_range = [
+                start_date + datetime.timedelta(days=i*increment) for i in range(range_length)
+            ]
+            test_date_range = dates.get_date_range(start_date, end_date, increment)
+            self.assertEqual(
+                range_length,
+                len(test_date_range),
+            )
+            for i in range(len(truth_date_range)):
+                self.assertEqual(
+                    truth_date_range[i],
+                    test_date_range[i]
+                )
+
+    def test_format_date_from_get_date_range(self):
+        start_date = datetime.date(2019, 10, 1)
+        end_date = start_date + datetime.timedelta(days=3)
+        truth_date_range = [
+            '2019-10-01',
+            '2019-10-02',
+            '2019-10-03',
+        ]
+        test_date_range = dates.get_date_range(
+            start_date,
+            end_date,
+            output_format='%Y-%m-%d'
+        )
+        self.assertEqual(
+            len(truth_date_range),
+            len(test_date_range),
+        )
+        for i in range(len(truth_date_range)):
+            self.assertEqual(
+                truth_date_range[i],
+                test_date_range[i]
+            )
