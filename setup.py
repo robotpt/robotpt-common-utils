f325a31beb48cc00fecd9f36fca2e61582ff2ba8
Audrow Nash <audrowna@usc.edu>
2019-10-21 13:59:12 -0700

    Update setuptools

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@ from distutils.core import setup
 setup(
   name='robotpt-common-utils',
   packages=['robotpt_common_utils'],
-  version='0.0.1',
+  version='0.0.2',
   license='MIT',
   description='Common python functions used in RobotPT projects',
   author='Audrow Nash',
   author_email='audrowna@usc.edu',
   url='https://github.com/robotpt/robotpt-common-utils',
+  setup_requires=[
+    'wheel',
+  ],
   install_requires=[
     'pandas',
   ],
