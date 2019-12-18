f3e370f7175ecb0eaa0b0a7a3a9a4f91de21060a
Audrow <audrowna@usc.edu>
2019-12-17 23:18:37 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/robotpt/robotpt-common-utils'
 EMAIL = 'audrowna@usc.edu'
 AUTHOR = 'Audrow Nash'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.7'
+VERSION = '0.0.8'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
